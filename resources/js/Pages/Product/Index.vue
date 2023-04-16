5615d30d5e0d588f3fecc11f2d42c70c7a930c26
destroylord <masapin68@gmail.com>
2023-04-16 07:24:21 +0700

    add intertiajs and testing page

---
+++ resources/js/Pages/Product/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    product: Object
+})
+
+</script>
+<template>
+    <Head title="List Product" />
+    <table style="border:1px solid black">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Deskripsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in product" :key="user.id">
+                    <td>{{user.name}} </td>
+                    <td>{{user.description}}</td>
+                </tr>
+            </tbody>
+        </table>
+</template>
